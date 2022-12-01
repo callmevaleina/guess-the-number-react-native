8177fb89bd74e392cdada54d9fc061932a4422ad
callmevaleina <valentina.crms@gmail.com>
2022-12-01 08:02:45 -0300

    My first commit

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Colors from '../../constants/colors';
+
+function NumberContainer({children}) {
+    return (
+        <View style={styles.numberContainer}>
+            <Text style={styles.numberText}>{children}</Text>
+        </View>
+    );
+};
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+    numberContainer: {
+        borderWidth: 4,
+        borderColor: Colors.accent500,
+        padding: 24,
+        margin: 24,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    numberText: {
+        color: Colors.accent500,
+        fontSize: 36,
+        //fontWeight: 'bold'
+        fontFamily: 'open-sans-bold'
+    }
+})
